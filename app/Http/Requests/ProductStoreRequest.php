30247ec0cbc6ca900456725b1ebea6986a53b2b1
Greg Rickaby <gregrickaby@gmail.com>
2024-01-21 13:12:45 -0600

    define product store request

---
+++ app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'description' => [
+                'required',
+                'string',
+            ],
+            'price' => [
+                'required',
+                'numeric',
+            ],
+            'image' => [
+                'required',
+                'image',
+                'dimensions:min_width=300,min_height=300',
+                'mimes:jpg,jpeg,png',
+            ],
+        ];
+    }
+}
