f928d7f495eb89b63aa1ccfe7d4e29cd1ce51a5e
Greg Rickaby <gregrickaby@gmail.com>
2024-01-22 14:19:06 -0600

    move buttons

--- resources/js/Pages/Products/{product}/View.vue
+++ resources/js/Pages/Products/{product}/View.vue
@@ -44,6 +44,11 @@ function destroy(id: number) {
                 </h2>
                 <div class="flex gap-4">
                     <Link
+                        :href="route('products.index')"
+                        class="focus:shadow-outline-blue rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-blue-700 focus:outline-none active:bg-blue-600"
+                        >Go Back</Link
+                    >
+                    <Link
                         :href="route('products.edit', product.id)"
                         class="focus:shadow-outline-blue rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-blue-700 focus:outline-none active:bg-blue-600"
                         >Edit Product</Link
@@ -54,11 +59,6 @@ function destroy(id: number) {
                     >
                         Delete
                     </button>
-                    <Link
-                        :href="route('products.index')"
-                        class="focus:shadow-outline-blue rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-blue-700 focus:outline-none active:bg-blue-600"
-                        >All Products</Link
-                    >
                 </div>
             </div>
         </template>
