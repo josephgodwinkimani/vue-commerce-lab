b79efbaca63a63116106380c0ade7c3a7a35d22a
Greg Rickaby <gregrickaby@gmail.com>
2024-01-23 22:06:34 -0600

    initial commit of create/edit views

--- resources/js/Pages/Orders/{order}/Edit.vue
+++ resources/js/Pages/Orders/{order}/Edit.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
 import FormOrder from '@/Components/Organisms/FormOrder.vue'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Order } from '@/types'
 import { Head, Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+
+const { order } = defineProps<{
+    order: Order
+}>()
 
 // Create a ref for the FormOrder component.
 const orderFormRef = ref()
 
-// Calls the submitForm method in FormOrder.
+// Create a ref for the saved state.
+const saved = ref(false)
+
+// Define the cancelOrDone computed property.
+const cancelOrDone = computed(() => {
+    return saved.value ? route('orders.index') : route('orders.show', order.id)
+})
+
+// Calls the submitForm method in orderForm.
 function saveOrder() {
     if (orderFormRef.value) {
         orderFormRef.value.submitForm()
+        saved.value = true
     }
 }
 </script>
 
 <template>
-    <Head title="Add New Order" />
+    <Head :title="`Edit Order ID: ${order.id}`" />
 
     <AuthenticatedLayout>
         <template #header>
@@ -24,7 +38,7 @@ function saveOrder() {
                 <h2
                     class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
                 >
-                    Add New Order
+                    Edit Order ID: {{ order.id }}
                 </h2>
                 <div class="flex gap-4">
                     <button
@@ -34,13 +48,17 @@ function saveOrder() {
                         Save
                     </button>
                     <Link
-                        :href="route('orders.index')"
+                        :href="cancelOrDone"
                         class="focus:shadow-outline-blue rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-blue-700 focus:outline-none active:bg-blue-600"
-                        >Cancel</Link
+                    >
+                        <span v-if="saved">Done</span
+                        ><span v-else>Cancel</span></Link
                     >
                 </div>
             </div>
         </template>
-        <FormOrder ref="orderFormRef" action="post" />
+        <div>
+            <FormOrder ref="orderFormRef" :order="order" action="patch" />
+        </div>
     </AuthenticatedLayout>
 </template>
