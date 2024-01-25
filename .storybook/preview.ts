61a229b297d03a9f1e34dc76ce66930fa0f25f2a
Greg Rickaby <gregrickaby@gmail.com>
2024-01-25 11:40:55 -0600

    storybook now toggles light/dark mode for Tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,17 @@
+import { withThemeByDataAttribute } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/vue3'
 import '../resources/css/app.css'
+
+export const decorators = [
+    withThemeByDataAttribute({
+        themes: {
+            light: 'light',
+            dark: 'dark'
+        },
+        defaultTheme: 'light',
+        attributeName: 'data-mode'
+    })
+]
 
 const preview: Preview = {
     parameters: {
