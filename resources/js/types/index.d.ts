7155d8d0b96a79fdbb500fa816a988db881ee8c5
Greg Rickaby <gregrickaby@gmail.com>
2024-01-26 14:18:40 -0600

    refactor types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -12,6 +12,8 @@ export interface Product {
     description: string
     id: number
     image: string
+    lifetime_revenue: number
+    lifetime_sales: number
     name: string
     price: number
     quantity: number
@@ -25,24 +27,26 @@ export interface Customer {
     created_at: string
     email: string
     id: number
+    lifetime_orders: number
+    lifetime_revenue: number
     name: string
-    order_count: number
-    orders: Order[]
     orders: Order[]
     phone: string
     state: string
-    total_spent: number
     updated_at: string
     zip: string
 }
 
 export interface OrderItem {
+    created_at: string
+    customer_id: number
     id: number
     order_id: number
-    price: number
     product: Product
     product_id: number
     quantity: number
+    total: number
+    updated_at: string
 }
 
 export interface Order {
@@ -50,19 +54,16 @@ export interface Order {
     created_at: string
     credit_card_last_four: string
     credit_card_type: string
-    customer: Customer
     customer_id: number
+    customer: Customer
     customer_note?: string
     id: number
     items: OrderItem[]
+    quantity: number
     shipping_address: string
     status: string
-    total_amount: number
+    total_revenue: number
     updated_at: string
-}
-
-export interface TopCustomer extends Customer {
-    total_spent: number
 }
 
 export type PageProps<
