d7d2157298f2d7d5faf3cac887f994049f829b8b
Greg Rickaby <gregrickaby@gmail.com>
2024-02-01 11:09:53 -0600

    add stylelint-vue support

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['stylelint-config-standard'],
+    extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
     rules: {
         'at-rule-no-unknown': [
             true,
